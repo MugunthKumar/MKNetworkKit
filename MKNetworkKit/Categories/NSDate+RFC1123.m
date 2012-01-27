b8139d53321343471d6293237152226051b0f2a0
Mugunth <mugunth.kumar@gmail.com>
2012-01-27 21:17:25 +0800

    GCD race condition bug fix

--- MKNetworkKit/Categories/NSDate+RFC1123.m
+++ MKNetworkKit/Categories/NSDate+RFC1123.m
@@ -14,38 +14,48 @@
 +(NSDate*)dateFromRFC1123:(NSString*)value_
 {
     if(value_ == nil)
-        return nil;
-    static NSDateFormatter *rfc1123 = nil;
-    if(rfc1123 == nil)
-    {
-        rfc1123 = [[NSDateFormatter alloc] init];
-        rfc1123.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
-        rfc1123.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
-        rfc1123.dateFormat = @"EEE',' dd MMM yyyy HH':'mm':'ss z";
+        return nil;    
+    
+    __strong static NSDateFormatter *rfc1123 = nil;
+    if (!rfc1123) {
+        static dispatch_once_t oncePredicate;
+        dispatch_once(&oncePredicate, ^{
+            rfc1123 = [[NSDateFormatter alloc] init];
+            rfc1123.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+            rfc1123.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+            rfc1123.dateFormat = @"EEE',' dd MMM yyyy HH':'mm':'ss z";
+        });
     }
     NSDate *ret = [rfc1123 dateFromString:value_];
     if(ret != nil)
         return ret;
     
     static NSDateFormatter *rfc850 = nil;
-    if(rfc850 == nil)
+    if(!rfc850)
     {
-        rfc850 = [[NSDateFormatter alloc] init];
-        rfc850.locale = rfc1123.locale;
-        rfc850.timeZone = rfc1123.timeZone;
-        rfc850.dateFormat = @"EEEE',' dd'-'MMM'-'yy HH':'mm':'ss z";
+        static dispatch_once_t oncePredicate;
+        dispatch_once(&oncePredicate, ^{
+            rfc850 = [[NSDateFormatter alloc] init];
+            rfc850.locale = rfc1123.locale;
+            rfc850.timeZone = rfc1123.timeZone;
+            rfc850.dateFormat = @"EEEE',' dd'-'MMM'-'yy HH':'mm':'ss z";
+        });
     }
     ret = [rfc850 dateFromString:value_];
     if(ret != nil)
         return ret;
     
     static NSDateFormatter *asctime = nil;
-    if(asctime == nil)
+    if(!asctime)
     {
-        asctime = [[NSDateFormatter alloc] init];
-        asctime.locale = rfc1123.locale;
-        asctime.timeZone = rfc1123.timeZone;
-        asctime.dateFormat = @"EEE MMM d HH':'mm':'ss yyyy";
+        static dispatch_once_t oncePredicate;
+        dispatch_once(&oncePredicate, ^{
+            
+            asctime = [[NSDateFormatter alloc] init];
+            asctime.locale = rfc1123.locale;
+            asctime.timeZone = rfc1123.timeZone;
+            asctime.dateFormat = @"EEE MMM d HH':'mm':'ss yyyy";
+        });
     }
     return [asctime dateFromString:value_];
 }
@@ -53,12 +63,15 @@
 -(NSString*)rfc1123String
 {
     static NSDateFormatter *df = nil;
-    if(df == nil)
+    if(!df)
     {
-        df = [[NSDateFormatter alloc] init];
-        df.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
-        df.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
-        df.dateFormat = @"EEE',' dd MMM yyyy HH':'mm':'ss 'GMT'";
+        static dispatch_once_t oncePredicate;
+        dispatch_once(&oncePredicate, ^{
+            df = [[NSDateFormatter alloc] init];
+            df.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US"];
+            df.timeZone = [NSTimeZone timeZoneWithAbbreviation:@"GMT"];
+            df.dateFormat = @"EEE',' dd MMM yyyy HH':'mm':'ss 'GMT'";
+        });
     }
     return [df stringFromDate:self];
 }
