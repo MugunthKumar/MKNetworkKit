c84859db68df104c72e44084a324f832fe3975b3
Mugunth <mugunth.kumar@gmail.com>
2012-04-15 11:35:22 +0800

    Added engine for S3

---
+++ MKNetworkKit/MKS3Engine.h
@@ -0,0 +1,13 @@
+//
+//  MKS3Engine.h
+//  MKNetworkKit-iOS
+//
+//  Created by Mugunth Kumar on 15/4/12.
+//  Copyright (c) 2012 Steinlogic. All rights reserved.
+//
+
+#import "MKNetworkEngine.h"
+
+@interface MKS3Engine : MKNetworkEngine
+
+@end
---
+++ MKNetworkKit/MKS3Engine.m
@@ -0,0 +1,13 @@
+//
+//  MKS3Engine.m
+//  MKNetworkKit-iOS
+//
+//  Created by Mugunth Kumar on 15/4/12.
+//  Copyright (c) 2012 Steinlogic. All rights reserved.
+//
+
+#import "MKS3Engine.h"
+
+@implementation MKS3Engine
+
+@end
