598df4a33d30f4884b9256ae2f0d9b0cc2e2b019
Evan <emkinney@gmail.com>
2012-08-04 18:12:11 -0400

    Removed call to dispatch_retain for iOS 6.0/OS X 10.8 and above

--- MKNetworkKit/MKNetworkKit.h
+++ MKNetworkKit/MKNetworkKit.h
@@ -33,9 +33,19 @@
 #if TARGET_OS_IPHONE
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000
+#define DO_GCD_RETAIN_RELEASE 0
+#else
+#define DO_GCD_RETAIN_RELEASE 1
+#endif
 #elif TARGET_OS_MAC
 #import <Cocoa/Cocoa.h>
 #import <AppKit/AppKit.h>
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#define DO_GCD_RETAIN_RELEASE 0
+#else
+#define DO_GCD_RETAIN_RELEASE 1
+#endif
 #endif
 
 #ifdef DEBUG
