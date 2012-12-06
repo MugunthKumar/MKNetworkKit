7d050dea4f736c2e64b1094ea7352af859df7a02
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2012-12-06 23:17:04 +0300

    added some test on creating operations

--- MKNetworkKit-Tests/MKNetworkKit-Tests/MKNetworkEngineSpec.m
+++ MKNetworkKit-Tests/MKNetworkKit-Tests/MKNetworkEngineSpec.m
@@ -2,10 +2,61 @@
 
 SPEC_BEGIN(MKNetworkEngineSpec)
 
+static NSString *const kMKTestHostName = @"example.com";
+static NSString *const kMKTestApiPath = @"api/v1";
+static NSString *const kMKTestPath = @"foo";
+
 describe(@"Network Engine", ^{
-    it(@"should be initialized", ^{
-        MKNetworkEngine *engine = [[MKNetworkEngine alloc] init];
-        [[engine should] beNonNil];
+    context(@"with hostname", ^{
+        __block MKNetworkEngine *engine = nil;
+        beforeEach(^{
+            engine = [[MKNetworkEngine alloc] initWithHostName:kMKTestHostName];
+        });
+        it(@"should return operation with appropriate hostname", ^{
+            MKNetworkOperation *op = [engine operationWithPath:kMKTestPath];
+            [[op.readonlyRequest.URL.host should] equal:kMKTestHostName];
+        });
+
+        it(@"should return operation with appropriate port", ^{
+            engine.portNumber = 8080;
+            MKNetworkOperation *op = [engine operationWithPath:kMKTestPath];
+            [[[op.readonlyRequest.URL port] should] equal:@8080];
+        });
+        it(@"should return operation with GET method by default", ^{
+            MKNetworkOperation *op = [engine operationWithPath:kMKTestPath];
+            [[[op.readonlyRequest HTTPMethod] should] equal:@"GET"];
+        });
+
+        it(@"should return operation with POST", ^{
+            MKNetworkOperation *op = [engine operationWithPath:kMKTestPath params:nil httpMethod:@"POST"];
+            [[[op.readonlyRequest HTTPMethod] should] equal:@"POST"];
+        });
+
+        it(@"should return operation with https scheme", ^{
+            MKNetworkOperation *op = [engine operationWithPath:kMKTestPath params:nil httpMethod:@"GET" ssl:YES];
+            [[[op.readonlyRequest.URL scheme] should] equal:@"https"];
+        });
+
+        it(@"should return operation with appropriate query", ^{
+            NSMutableDictionary *params = [NSMutableDictionary dictionaryWithObject:@"bar" forKey:@"foo"];
+            MKNetworkOperation *op = [engine operationWithPath:kMKTestPath params:params];
+            [[[op.readonlyRequest.URL query] should] equal:@"foo=bar"];
+        });
+    });
+
+    it(@"should return operation with appropriate custom header", ^{
+        MKNetworkEngine *engine = [[MKNetworkEngine alloc] initWithHostName:kMKTestHostName
+                                                         customHeaderFields:@{ @"Some-Header" : @"Bar" }];
+        MKNetworkOperation *op = [engine operationWithPath:kMKTestPath];
+        [[op.readonlyRequest.allHTTPHeaderFields[@"Some-Header"] should] equal:@"Bar"];
+    });
+
+    it(@"should return operation with appropriate apiPath", ^{
+        MKNetworkEngine *engine = [[MKNetworkEngine alloc] initWithHostName:kMKTestHostName
+                                                                    apiPath:kMKTestApiPath
+                                                         customHeaderFields:nil];
+        MKNetworkOperation *op = [engine operationWithPath:kMKTestPath];
+        [[[op.readonlyRequest.URL path] should] equal:[NSString stringWithFormat:@"/%@/%@", kMKTestApiPath, kMKTestPath]];
     });
 });
 
