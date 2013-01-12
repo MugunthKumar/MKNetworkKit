45b1dd6481d5d1636a0b1b442ba0429c679d25ca
Mugunth <mugunth.kumar@gmail.com>
2013-01-12 13:24:02 +0800

    Images will be cached even if they are on secure server

--- MKNetworkKit/MKNetworkEngine.m
+++ MKNetworkKit/MKNetworkEngine.m
@@ -476,7 +476,8 @@ static NSOperationQueue *_sharedNetworkQueue;
     }
   
   MKNetworkOperation *op = [self operationWithURLString:[url absoluteString]];
-  
+  op.shouldCacheResponseEvenIfProtocolIsHTTPS = YES;
+
   [op addCompletionHandler:^(MKNetworkOperation *completedOperation) {
     
     if (imageFetchedBlock)
@@ -509,7 +510,8 @@ static NSOperationQueue *_sharedNetworkQueue;
     }
   
   MKNetworkOperation *op = [self operationWithURLString:[url absoluteString]];
-  
+  op.shouldCacheResponseEvenIfProtocolIsHTTPS = YES;
+
   [op addCompletionHandler:^(MKNetworkOperation *completedOperation) {
     [completedOperation decompressedResponseImageOfSize:size
                                       completionHandler:^(UIImage *decompressedImage) {
