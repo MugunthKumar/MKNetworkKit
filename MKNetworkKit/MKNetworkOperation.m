b80e365acd066d7fb9e7e9d8b18cbad68cc1c543
Mugunth <mugunth.kumar@gmail.com>
2013-12-13 22:32:24 +0800

    Minor fix to the format specifier

--- MKNetworkKit/MKNetworkOperation.m
+++ MKNetworkKit/MKNetworkOperation.m
@@ -1509,7 +1509,7 @@ totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite {
     errorBlock(self, error);
   
 #if TARGET_OS_IPHONE
-  DLog(@"State: %d", [[UIApplication sharedApplication] applicationState]);
+  DLog(@"State: %ld", [[UIApplication sharedApplication] applicationState]);
   if([[UIApplication sharedApplication] applicationState] == UIApplicationStateBackground)
     [self showLocalNotification];
 #endif
